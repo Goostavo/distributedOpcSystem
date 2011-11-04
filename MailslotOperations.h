ce3c12ab18dabf8e6e67c64650f0cf0f524624f5
Rafael Gurgel <rafa.gurgel@gmail.com>
2011-11-04 21:36:21 -0200

    corrigindo comentarios

--- MailslotOperations.h
+++ MailslotOperations.h
@@ -1,19 +1,18 @@
 // Mailslot Operations
 //
-// This functions are a modified version of the "Reading from a Mailslot" 
-// and "Writing from a Mailslot" that was originally available at Microsoft
-// Developer Network Platforms (MSND) where are examples of Using Mailslots
+// Thess functions are a modified version of the "Reading from a Mailslot" 
+// and "Writing from a Mailslot", originally available at Microsoft
+// Developer Network Platforms (MSDN).
 //
-// Some functions that wasn't necessary at this project were removed and 
-// another adaptations were done. 
+// Some functions that wasn't necessary for this project were removed and 
+// some adaptations were done. 
 //
-// The based Mailslots tutorial can be found in
+// The Mailslots tutorial used can be found in
 //      http://msdn.microsoft.com/en-us/library/windows/desktop/aa365794.aspx
 //
-//
+//s
 // Gustavo Barreto Garcia
 // Rafael Gilmar Ribeiro Gurgel
-
 #include <windows.h>
 
 BOOL WriteSlot(HANDLE hSlot, LPTSTR lpszMessage);
