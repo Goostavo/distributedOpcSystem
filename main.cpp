1de5d318d26e9ce23963b8c1b2dc30c593d36c5e
Gustavo Barreto <goostavobg@gmail.com>
2011-11-04 03:15:32 -0200

    Adicionado funcoes de teste no main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@
 //Rafael Gilmar Ribeiro Gurgel	2006048192
 
 #include <iostream>
+#include "Opcclass.h"
 
 int main (void)
 {
 	std::cout << "Hello World!" << std::endl;
+	Opcclass Opc_tp = new Opcclass();			//Instancia Classe OPC
+	Opc_tp.AddGroup();
+	Opc_tp.AddItem();
 	system("pause");
 	return 0;
 }
