90a30c572129f061bb5af6830f7b842ee8398b8d
Gustavo Barreto <goostavobg@gmail.com>
2011-11-04 01:52:07 -0200

    -Adicionados arquivos base do OPCclass

---
+++ Opcclass.h
@@ -0,0 +1,17 @@
+// Simple OPC Client Class
+//
+// This is a modified version of the "Simple OPC Client" originally
+// developed by Philippe Gras (CERN) and further enhanced by Luiz T. S.
+// Mendes (UFMG).
+//
+// The code, previously monolitic was edited with the objective to be 
+// fully encapsulated inside a class.
+// The public functions are based in objective of use instead of being
+// based in the main OPC COM functions.
+
+class Opcclass
+{
+	public:
+			Opcclass();
+			~Opcclass();
+}
